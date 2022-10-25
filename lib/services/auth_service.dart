41bda995f33cdc1a91ae19159f15a9b828aa14d2
Javier David <javierdgarciav@gmail.com>
2022-10-24 23:12:06 -0500

    grabar token de seguridad

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier{
   final String _baseUrl = "identitytoolkit.googleapis.com";
   final String _firebaseToken = "AIzaSyA7A-FDNlj9UH2I2jXMXW6bj5CxaV_xiX8";
+  final storage = new FlutterSecureStorage();
+
   //Si retornamos algo es un error, sino todo bien
   Future<String?> createUser(String email, String password) async{
     final Map<String, dynamic> authData = {
@@ -22,6 +25,7 @@ class AuthService extends ChangeNotifier{
     final Map<String, dynamic> decodeResp = json.decode(resp.body);
     
     if(decodeResp.containsKey('idToken')){
+      storage.write(key: 'token', value: decodeResp['idToken']);
       //Token hay que guardarlo en un lugar seguro
       return null;
     }else{
@@ -44,9 +48,17 @@ class AuthService extends ChangeNotifier{
     final Map<String, dynamic> decodeResp = json.decode(resp.body);
     if(decodeResp.containsKey('idToken')){
       //Token hay que guardarlo en un lugar seguro
+      storage.write(key: 'token', value: decodeResp['idToken']);
       return null;
     }else{
       return decodeResp['error']['message'];
     }
   }
+
+  Future logout() async {
+    await storage.delete(key: 'token');
+    return;
+  }
+
+
 }
